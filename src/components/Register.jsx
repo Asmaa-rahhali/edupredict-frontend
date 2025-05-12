60c952531326172028ababf9b21a464c8c8d9175
Asmaa <asmaarahhali1@gmail.com>
2025-05-12 20:53:47 +0100

    C8

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
-import { REGISTER_URL } from "../api";
 
 export default function Register() {
   const [form, setForm] = useState({
@@ -58,7 +57,8 @@ export default function Register() {
     setServerError("");
 
     try {
-      await axios.post(REGISTER_URL, form);
+      console.log("Formulaire envoyé :", form);
+      await axios.post("https://edupredict-backend.onrender.com/api/accounts/register/", form);
       navigate("/login");
     } catch (err) {
       const apiError =
